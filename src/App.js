5bbd9f16f6c1b91939c5486481b05d5a94bb6456
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2022-08-28 10:29:26 +0800

    Frontend

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css'
+import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './routes/Home';
+import Statistics from './routes/Statistics'
+import Login from './routes/Login'
+import Signup from './routes/Signup'
+
+export default function App(){
+  return(
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="statistics" element={<Statistics/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="signup" element={<Signup/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
