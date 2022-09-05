b016585969fe5995234c85d46f075594b9fe4132
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2022-09-05 10:43:57 +0800

    Sign up page working

--- backend/api/database/database.js
+++ backend/api/database/database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'sql.freedb.tech', // Replace with your host name
-  user: 'freedb_sip_db_admin',      // Replace with your database username
-  password: 'Jp@SRCvz&t4$apV',      // Replace with your database password
-  database: 'freedb_sip_db' // // Replace with your database Name
+  host: 'remotemysql.com', // Replace with your host name
+  user: 'UZKSgJUOhh',      // Replace with your database username
+  password: '5BAoIVDdi5',      // Replace with your database password
+  database: 'UZKSgJUOhh' // // Replace with your database Name
 }); 
  
 conn.connect(function(err) {
