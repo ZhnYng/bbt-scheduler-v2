71db9dc60a97c1f01129535ce7b103ede6d467da
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2022-12-16 18:37:04 +0800

    updates

---
+++ sip_db_schema.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS `sip_db`;
+
+use sip_db;
+
+CREATE TABLE users (
+	user_id int NOT NULL AUTO_INCREMENT,
+    username varchar(225) NOT NULL,
+    password varchar(225) NOT NULL,
+    last_updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (user_id),
+    UNIQUE (username)
+);
+
+CREATE TABLE goal (
+	goal_id int NOT NULL AUTO_INCREMENT,
+    goal int NOT NULL,
+    user_id int,
+    last_updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (goal_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE drinks (
+	drinks_id int NOT NULL AUTO_INCREMENT,
+    drinks int NOT NULL,
+    user_id int,
+	last_updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (drinks_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
